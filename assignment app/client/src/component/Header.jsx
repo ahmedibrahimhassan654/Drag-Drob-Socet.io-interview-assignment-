10964c3acf5baf4eaf9e1c18ac3a1f41dc7194a3
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-10-23 00:31:05 +0200

    add emojie

--- assignment app/client/src/component/Header.jsx
+++ assignment app/client/src/component/Header.jsx
@@ -6,7 +6,9 @@ const Header = ({ logout }) => {
   return (
     <>
       <div className={"row"}>
-        <p className={"page-header"}>Drag & drob & socket io assignment 🔥</p>
+        <p className={"page-header"}>
+          Drag & drob & socket io assignment 🔥 ✌️{" "}
+        </p>
         <h1 className="text-center text-success">{`welcom player ${userName} in room number ${room}`}</h1>
         <button
           className="btn btn-primary btn-sm btn-block mt-3"
