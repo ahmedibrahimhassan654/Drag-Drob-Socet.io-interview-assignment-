031bf471e2c0ba20119aae019e20dba91d9151c4
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-10-24 22:41:05 +0200

    edit header

--- assignment app/client/src/component/Header.jsx
+++ assignment app/client/src/component/Header.jsx
@@ -5,18 +5,21 @@ const Header = ({ logout }) => {
   const room = window.localStorage.getItem("room");
   return (
     <>
-      <div className={"row"} style={{ position: "sticky", top: 0 }}>
+      <div className={"row"} style={{ position: "sticky" }}>
         <p className={"page-header"}>
-          Drag & drob & socket io assignment 🔥 ✌️{" "}
+          Drag & drob & socket io assignment 🔥 ✌️ <br />
+          {`welcom player ${userName} in room number ${room}`}
+          <br />
+          <button
+            className="btn btn-primary btn-sm btn-block mt-3"
+            type="button"
+            onClick={logout}
+          >
+            logout
+          </button>
+          <br />
+          <span>Created By Ahmed Ibrahim</span>
         </p>
-        <h1 className="text-center text-light ">{`welcom player ${userName} in room number ${room}`}</h1>
-        <button
-          className="btn btn-primary btn-sm btn-block mt-3"
-          type="button"
-          onClick={logout}
-        >
-          logout
-        </button>
       </div>
     </>
   );
